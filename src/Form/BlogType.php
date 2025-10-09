f81eb658025778fcb9dd2962bf3935dd9152d73e
kouider ayad walid <kouiderayadwalid@gmail.com>
2025-10-09 16:49:31 +0200

    add text editor

--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Blog;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +16,24 @@ class BlogType extends AbstractType
     public function buildForm (FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', null, [
+                'required' => true,
+            ])
             ->add('created_at', null, [
                 'widget' => 'single_text',
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'class' => 'js-editorjs',
+                    'hidden' => true,
+                ],
+            ])
+            ->add('coverImage', FileType::class, [
+                'mapped' => false,   // we'll move the file in the controller
+                'required' => false,
+                'constraints' => [
+                    new Image(maxSize: '4M', mimeTypesMessage: 'Please upload a valid image'),
+                ],
             ])
             ->add('is_published')
             ->add('likes')
