b96b1a817185cafce9eb7921bb62b8c5bd0c802f
kouider ayad walid <kouiderayadwalid@gmail.com>
2025-10-27 10:11:26 +0100

    scss adjusment

--- src/Twig/BlogCategoriesExtension.php
+++ src/Twig/BlogCategoriesExtension.php
@@ -3,23 +3,32 @@
 namespace App\Twig;
 
 use App\Repository\BlogCategoriesRepository;
+use App\Repository\BlogsRepository;
+use App\Repository\UserRepository;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
 
 class BlogCategoriesExtension extends AbstractExtension implements GlobalsInterface
 {
-    private BlogCategoriesRepository $repo;
+    private BlogCategoriesRepository $blogCategoriesRepository;
+    private UserRepository $userRepository;
+    private BlogsRepository $blogsRepository;
 
-    public function __construct (BlogCategoriesRepository $repo)
+    public function __construct (BlogCategoriesRepository $blogCategoriesRepository, UserRepository $userRepository, BlogsRepository $blogsRepository)
     {
-        $this->repo = $repo;
+        $this->blogCategoriesRepository = $blogCategoriesRepository;
+        $this->userRepository = $userRepository;
+        $this->blogsRepository = $blogsRepository;
     }
 
     public function getGlobals (): array
     {
         return [
             // this will be available in ALL twig templates
-            'blogCategories' => $this->repo->findAll(),
+            'blogCategories' => $this->blogCategoriesRepository->findAll(),
+            'blogCategoriesCount' => $this->blogCategoriesRepository->count(),
+            'blogsCount' => $this->blogsRepository->count(),
+            'usersCount' => $this->userRepository->count(),
         ];
     }
 }
